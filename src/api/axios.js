81c35227288da42ef9ea90eaa99ae2531a5a0aca
Kinan Hatahet <kinan.hatahet@gmail.com>
2023-03-02 21:20:40 +0000

    fixing chartPage to show max value

--- src/api/axios.js
+++ src/api/axios.js
@@ -28,22 +28,12 @@ export const getDeviceLists = async (pageParam = 1) => {
   return response.data;
 };
 
-export const getHistoricChartData = async (device1, ...others) => {
-  let link;
-  // One device selected
-  if (others.length === 0) {
-    link = `https://user.windcrane.com/manager/api/v1/historic?s=${device1},A1,lastmax&r=week&i=minute&n=1`;
-  }
-  // Multiple devices selected
-  else if (others.length > 0) {
-    const multipleDevices = others.map(device => `;${device},A1,generated`);
-    link = `https://user.windcrane.com/manager/api/v1/historic?s=${device1},A1,lastmax${multipleDevices.map(d => d)}&r=week&i=minute&n=1`;
-    // user.windcrane.com/manager/api/v1/historic?s=580055403725,A1,lastmax;580055402602,A1,generated&r=week&i=minute&n=1/
-    // user.windcrane.com/manager/api/v1/historic?s=580055403725,A1,lastmax;580055402602,A1,generated,;580053212383,A1,generated&r=week&i=minute&n=1
-  }
+export const getHistoricChartData = async device => {
+  const link = `https://user.windcrane.com/manager/api/v1/historic?s=${device},A1,lastmax&r=week&i=minute&n=1`;
   const response = await axios.get(link, {
     headers: { Authorization: `Bearer ${localStorage.getItem('accessToken')}` },
     withCredentials: true
   });
+  // console.log(response.data)
   return response.data;
 };
--- src/components/ChartPage.jsx
+++ src/components/ChartPage.jsx
@@ -4,29 +4,61 @@ import * as am5xy from '@amcharts/amcharts5/xy';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 import { getHistoricChartData } from '../api/axios';
 
+// This function is used to combine the data from the different devices.
+// https://stackoverflow.com/questions/53224923/merge-array-of-objects-by-object-key
+// https://stackoverflow.com/questions/71048528/how-to-merge-two-arrays-of-objects-in-javascript
+
+const COMBINE = data =>
+  data.reduce((acc, val) => {
+    const i = acc.findIndex(o => o.date === val.date);
+    if (i !== -1) {
+      acc[i] = { ...acc[i], ...val };
+      return acc;
+    } else return acc.concat(val);
+  }, []);
+
 const ChartPage = ({ setView, selectedDevices, setSelectedDevices }) => {
   const [chartData, setChartData] = useState([]);
+  const [devicesArray, setDevicesArray] = useState([]);
 
   // The main function to fetch the devices according to the page number.
   const getData = async devices => {
     try {
-      const data = await getHistoricChartData(...devices);
-      // console.log(chartData)
-      setChartData(data);
-      setSelectedDevices([]);
+      // The data from the devices is stored in these arrays.
+      let devicesMax = [];
+      let devicesImei = [];
+
+      devices.map(async device => {
+        // The data is fetched from the API.
+        const data = await getHistoricChartData(device.imei);
+        // The data is mapped to the device imei. This is used to create the legend.
+        const deviceLabel = device.imei + '_max';
+        const dataNameChange = data.map(data => {
+          return { date: data.date, [deviceLabel]: data.max };
+        });
+
+        devicesMax.push(...dataNameChange);
+        devicesImei.push(deviceLabel);
+
+        // The data is combined and set to the chartData state.
+        setChartData(COMBINE(devicesMax));
+        setDevicesArray(devicesImei);
+        setSelectedDevices([]);
+      });
     } catch (err) {
       console.log(err?.response);
     }
   };
 
-  // The fetch function runs everytime the page number changes.
+ 
   useEffect(() => {
-    getData(selectedDevices.map(device => device.imei));
+    getData(selectedDevices);
   }, []);
 
   useLayoutEffect(() => {
     // Create root element
     let root = am5.Root.new('chartdiv');
+
 
     // Set themes
     // https://www.amcharts.com/docs/v5/concepts/themes/
@@ -111,9 +143,9 @@ const ChartPage = ({ setView, selectedDevices, setSelectedDevices }) => {
       series.appear(1000);
     }
 
-    createSeries('std', 'std');
-    createSeries('max', 'max');
-    createSeries('avg', 'avg');
+    devicesArray.map(data => {
+      createSeries(data, data);
+    });
 
     // Add scrollbar
     // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
@@ -127,6 +159,7 @@ const ChartPage = ({ setView, selectedDevices, setSelectedDevices }) => {
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
     chart.appear(1000, 100);
+    
 
     // Add legend
     let legend = chart.children.push(
@@ -137,8 +170,11 @@ const ChartPage = ({ setView, selectedDevices, setSelectedDevices }) => {
     );
     legend.data.setAll(chart.series.values);
 
+
     return () => {
       root.dispose();
+      
+   
     };
   }, [chartData]);
 
@@ -153,7 +189,6 @@ const ChartPage = ({ setView, selectedDevices, setSelectedDevices }) => {
         </div>
       </div>
       <div className="mt-10" id="chartdiv" style={{ width: '100%', height: '500px' }}></div>
-      
     </div>
   );
 };
